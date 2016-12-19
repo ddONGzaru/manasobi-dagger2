d598e412c809179b0cc5230a9b7ecbec3586980f
tw.jang <tw.jang@dream-ant.com>
2016-12-19 16:15:31 +0900

    mvp_dagger2_retrofit_rxandroid sample

--- mvp_dagger_retrofit_rxandroid_rxbinding/src/main/java/io/manasobi/ui/activity/MainActivity.java
+++ mvp_dagger_retrofit_rxandroid_rxbinding/src/main/java/io/manasobi/ui/activity/MainActivity.java
@@ -3,8 +3,11 @@ package io.manasobi.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.jakewharton.rxbinding.view.RxView;
 
 import javax.inject.Inject;
 
@@ -17,6 +20,10 @@ import io.manasobi.di.DaggerExchangeRateComponent;
 import io.manasobi.di.ExchangeRateComponent;
 import io.manasobi.domain.entity.ExchangeRateResponse;
 import io.manasobi.presenter.ExchangeRatePresenter;
+import retrofit2.adapter.rxjava.HttpException;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements ExchangeRatePresenter.View {
 
@@ -33,12 +40,49 @@ public class MainActivity extends AppCompatActivity implements ExchangeRatePrese
         component().inject(this);
         presenter.setView(this);
         ButterKnife.bind(this);
+
+        onClickButtonRx();
     }
 
+
+    void onClickButtonRx() {
+
+        RxView.clicks(button)
+              .switchMap(event -> presenter.getExchangeRateRx())
+                                           .doOnError(this::showError)
+                                           .onErrorResumeNext(Observable.empty())
+              .observeOn(AndroidSchedulers.mainThread())
+              .subscribe(res -> {
+                  textView.setText("[KRW] :: " + String.valueOf(res.getRates().getKRW()));
+              }, throwable -> {
+                  Log.d("通信 -> ", "失敗" + throwable.getMessage());
+                  Timber.e("Error: " + throwable.getMessage());
+                  throwable.printStackTrace();
+              });
+
+    }
+/*
     @OnClick(R.id.button)
     void onClickButton() {
         presenter.getExchangeRate();
     }
+*/
+
+    private void showError(Throwable t) {
+
+        Timber.e(t.getMessage());
+
+        String message = t.getMessage();
+        if (t instanceof HttpException) {
+            HttpException httpException = (HttpException) t;
+            if (httpException.code() == 403)
+                message = "403 에러";
+            else if (httpException.code() == 404)
+                message = "404 에러";
+        }
+        textView.setText(message);
+    }
+
 
     @Override
     public void bindExchangeRate(ExchangeRateResponse exchangeRateResponse) {
